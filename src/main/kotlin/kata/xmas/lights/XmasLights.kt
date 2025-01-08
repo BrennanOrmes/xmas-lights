062d883702d27c2a5a59450cc9a88fb8c9be4e5b
Brennan Ormes <brennanormes@gmail.com>
2025-01-08 18:02:16 +0000

    added new functions and tests

--- src/main/kotlin/kata/xmas/lights/XmasLights.kt
+++ src/main/kotlin/kata/xmas/lights/XmasLights.kt
@@ -1,14 +1,72 @@
 package kata.xmas.lights
 
-class XmasLights {
-    fun greeting() = "Hello World"
+class XmasLights(
+
+) {
+    constructor(allOn: Boolean) : this() {
+        setUpLights(allOn)
+    }
+
+    private val lights = hashMapOf<Pair<Int, Int>, Light>()
+
+    fun getLights(): HashMap<Pair<Int, Int>, Light> {
+        return lights
+    }
+
+    fun turnOnRange(from : Pair<Int, Int>, to : Pair<Int, Int>) {
+        for (x in from.first..to.first) {
+            for (y in from.second..to.second) {
+                turnOn(x, y)
+            }
+        }
+    }
+
+    fun turnOn(x: Int, y: Int): Light {
+        val light = getLight(x, y)
+
+        light.isOn = true
+
+        return light
+    }
+
+    fun turnOff(x: Int, y: Int): Light {
+        val light = getLight(x, y)
+
+        light.isOn = false
+
+        return light
+    }
+
+    fun toggle(x: Int, y: Int): Light {
+        val light = getLight(x, y)
+
+        light.isOn = !light.isOn
+
+        return light
+    }
+
+    private fun setUpLights(isOn: Boolean) {
+        for (x in 0..999) {
+            for (y in 0..999) {
+                lights[Pair(x, y)] = Light(x, y, isOn)
+            }
+        }
+    }
+
+    private fun getLight(x: Int, y: Int): Light {
+        return lights[Pair(x, y)]!!
+    }
 }
+
+class Light(
+    val x: Int,
+    val y: Int,
+    var isOn: Boolean
+)
 
 fun main(vararg args: String) {
     for(arg in args) {
         println(arg)
     }
-    println(XmasLights().greeting())
 }
-
 
--- src/test/kotlin/kata/xmas/lights/XmasLightsTest.kt
+++ src/test/kotlin/kata/xmas/lights/XmasLightsTest.kt
@@ -2,10 +2,116 @@ package kata.xmas.lights
 
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+import org.junit.jupiter.params.provider.ValueSource
 
 internal class XmasLightsTest {
 
-    @Test fun greeting() {
-        assertEquals("Hello World", XmasLights().greeting())
+
+
+    @ParameterizedTest
+    @CsvSource(
+        "0,0",
+        "1,2",
+        "2,3",
+        "3,4",
+        "4,5",
+        "5,6",
+        "6,7",
+        "7,8",
+        "8,9",
+        "9,10",
+    )
+    fun `should turn on lights`(
+        x: Int,
+        y: Int
+    ) {
+        val xmasLights = XmasLights(false)
+
+        val light = xmasLights.turnOn(x, y)
+
+        assertEquals(x, light.x)
+        assertEquals(y, light.y)
+        assertEquals(true, light.isOn)
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        "0,0",
+        "1,2",
+        "2,3",
+        "3,4",
+        "4,5",
+        "5,6",
+        "6,7",
+        "7,8",
+        "8,9",
+        "9,10",
+    )
+    fun `should turn off lights`(
+        x: Int,
+        y: Int
+    ) {
+        val xmasLights = XmasLights(true)
+
+        val result = xmasLights.turnOff(x, y)
+
+        assertEquals(x, result.x)
+        assertEquals(y, result.y)
+        assertEquals(false, result.isOn)
+    }
+
+    @Test
+    fun `should only turn off the lights expected lights`() {
+        val xmasLights = XmasLights(true)
+
+        xmasLights.turnOff(0, 0)
+
+        val otherLights = xmasLights.getLights().values.filter { it.x != 0 && it.y != 0 }
+        val light = xmasLights.getLights().values.first { it.x == 0 && it.y == 0 }
+
+        otherLights.forEach {
+            assertEquals(true, it.isOn)
+        }
+
+        assertEquals(false, light.isOn)
+    }
+
+    @ParameterizedTest
+    @ValueSource(
+        booleans = [true, false]
+    )
+    fun `should toggle 0,0 on or off`(
+        isOn: Boolean
+    ) {
+        val xmasLights = XmasLights(isOn)
+
+        xmasLights.toggle(0, 0)
+
+        val otherLights = xmasLights.getLights().values.filter { it.x != 0 && it.y != 0 }
+        val light = xmasLights.getLights().values.first { it.x == 0 && it.y == 0 }
+
+        otherLights.forEach {
+            assertEquals(isOn, it.isOn)
+        }
+
+        assertEquals(!isOn, light.isOn)
+    }
+
+    @Test
+    fun `should turn 0,0 to 999,999 to on`() {
+        val xmasLights = XmasLights(false)
+
+        xmasLights.turnOnRange(Pair(0, 0), Pair(999, 999))
+
+        xmasLights.getLights().values.forEach {
+            assertEquals(true, it.isOn)
+        }
+    }
+
+    @Test
+    fun `should turn 0,0 to 999,999 to off`() {
+
     }
 }
